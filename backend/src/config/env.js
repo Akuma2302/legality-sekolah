44e8cbf6325499618ec47248111b802226254f88
Akuma2302 <asyraaf2302@gmail.com>
2026-08-05 17:04:12 +0800

    password

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -7,4 +7,6 @@ export const env = {
   mongodbUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   frontendUrl: process.env.FRONTEND_URL || '*',
+  username: process.enc.DEFAULT_USERNAME,
+  password: process.enc.DEFAULT_PASSWORD,
 };
