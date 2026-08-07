aa5b3024cebe79c6d5e3b2cb6012d8dc3d07253b
Akuma2302 <asyraaf2302@gmail.com>
2026-08-07 15:44:43 +0800

    state and branch changes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,10 +1,10 @@
 export const SCHOOL_TYPES = ['A', 'B', 'C', 'New'];
 
-export const BRANCHES = ['Central', 'North', 'East Coast', 'Borneo'];
+export const BRANCHES = ['Tengah', 'Utara', 'Pantai Timur', 'Borneo'];
 
 export const STATES = [
   'Johor', 'Kedah', 'Kelantan', 'Melaka', 'Negeri Sembilan', 'Pahang', 'Perak',
-  'Perlis', 'Penang', 'Sabah', 'Sarawak', 'Selangor', 'Terengganu',
+  'Perlis', 'Penang', 'Sabah', 'Sarawak', 'Selangor', 'Terengganu', 'Putrajaya',
 ];
 
 export const LEGALITY_STATUS_STYLES = {
